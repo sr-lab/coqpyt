de3e37b4783a3988fc85aa170db3fbd5c203b492
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2023-09-05 14:39:39 +0100

    Fix test

--- tests/resources/test_type_class.v
+++ tests/resources/test_type_class.v
@@ -3,6 +3,7 @@ Class EqDecNew (A : Type) :=
 { eqb_new : A -> A -> bool ;
   eqb_leibniz_new : forall x y, eqb_new x y = true -> x = y ;
   eqb_ident_new : forall x, eqb_new x x = true }.
+End TypeClass.
 
 #[refine] Global Instance unit_EqDec : TypeClass.EqDecNew unit := { eqb_new x y := true }.
 Proof.

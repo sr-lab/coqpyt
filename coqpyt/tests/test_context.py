47ca6073a6952f06c18e712efd31a1962c7ee911
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2024-01-04 15:32:20 +0000

    handle notations without scope stored in the context

--- coqpyt/tests/test_context.py
+++ coqpyt/tests/test_context.py
@@ -12,3 +12,25 @@ def test_notation_colon_problem():
     context.update(mock_context)
     term = context.get_notation("_ : _", "")
     assert term == mock_context["x : y : type_scope"]
+
+
+def test_notation_unscoped():
+    context = FileContext("tests/tests.v")
+    mock_context = {
+        "x : y": Term(
+            Step("XXX", "YYY", None), TermType.NOTATION, "tests/tests.v", []
+        ),
+        "z : y : test_scope": Term(
+            Step("ZZZ", "WWW", None), TermType.NOTATION, "tests/tests.v", []
+        )
+    }
+    context.update(mock_context)
+
+    term = context.get_notation("_ : _", "type_scope")
+    assert term == mock_context["x : y"]
+
+    term = context.get_notation("_ : _", "")
+    assert term == mock_context["x : y"]
+
+    term = context.get_notation("_ : _", "test_scope")
+    assert term == mock_context["z : y : test_scope"]
